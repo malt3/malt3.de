cd8355b981b40e91bfac82a988909fca16883045
Malte Poll <malte.poll@web.de>
2014-04-02 20:00:48 +0200

    initial commit

---
+++ datasource.js
@@ -0,0 +1,43 @@
+var stringArray = new Array();
+stringArray[0] = "MMMMMMMM               MMMMMMMM                  lllllll         tttt           333333333333333</br>";
+stringArray[1] = "M:::::::M             M:::::::M                  l:::::l      ttt:::t          3:::::::::::::::33 </br>";
+stringArray[2] = "M::::::::M           M::::::::M                  l:::::l      t:::::t          3::::::33333::::::3</br>";
+stringArray[3] = "M:::::::::M         M:::::::::M                  l:::::l      t:::::t          3333333     3:::::3</br>";
+stringArray[4] = "M::::::::::M       M::::::::::M  aaaaaaaaaaaaa    l::::lttttttt:::::ttttttt                3:::::3</br>";
+stringArray[5] = "M:::::::::::M     M:::::::::::M  a::::::::::::a   l::::lt:::::::::::::::::t                3:::::3</br>";
+stringArray[6] = "M:::::::M::::M   M::::M:::::::M  aaaaaaaaa:::::a  l::::lt:::::::::::::::::t        33333333:::::3 </br>";
+stringArray[7] = "M::::::M M::::M M::::M M::::::M           a::::a  l::::ltttttt:::::::tttttt        3:::::::::::3  </br>";
+stringArray[8] = "M::::::M  M::::M::::M  M::::::M    aaaaaaa:::::a  l::::l      t:::::t              33333333:::::3 </br>";
+stringArray[9] = "M::::::M   M:::::::M   M::::::M  aa::::::::::::a  l::::l      t:::::t                      3:::::3</br>";
+stringArray[10] = "M::::::M    M:::::M    M::::::M a::::aaaa::::::a  l::::l      t:::::t                      3:::::3</br>";
+stringArray[11] = "M::::::M     MMMMM     M::::::Ma::::a    a:::::a  l::::l      t:::::t    tttttt            3:::::3</br>";
+stringArray[12] = "M::::::M               M::::::Ma::::a    a:::::a l::::::l     t::::::tttt:::::t3333333     3:::::3</br>";
+stringArray[13] = "M::::::M               M::::::Ma:::::aaaa::::::a l::::::l     tt::::::::::::::t3::::::33333::::::3</br>";
+stringArray[14] = "M::::::M               M::::::M a::::::::::aa:::al::::::l       tt:::::::::::tt3:::::::::::::::33 </br>";
+stringArray[15] = "MMMMMMMM               MMMMMMMM  aaaaaaaaaa  aaaallllllll         ttttttttttt   333333333333333   </br>";
+stringArray[16] = "</br>System is loading ";
+stringArray[17] = "</br>Please wait!";
+stringArray[18] = "Finished loading</br>";
+stringArray[19] = "Ver 0.1 beta</br>";
+stringArray[20] = "Welcome visitor. Please type in \"help\" and press Enter to get more information</br>";
+
+var sysInits = new Array();
+sysInits[0] = "nsa_bot.sh";
+
+var nyanArray = new Array();
+nyanArray[0] = "+      o     +              o     </br>";
+nyanArray[1] = "    +             o     +       + </br>";
+nyanArray[2] = "o          +                      </br>";
+nyanArray[3] = "    o  +           +        +     </br>";
+nyanArray[4] = "+        o     o       +        o </br>";
+nyanArray[5] = "-_-_-_-_-_-_-_,------,      o     </br>";
+nyanArray[6] = "_-_-_-_-_-_-_-|   /\\_/\\           </br>";
+nyanArray[7] = "-_-_-_-_-_-_-~|__( ^ .^)  +     + </br>";
+nyanArray[8] = "_-_-_-_-_-_-_-\"\"  \"\"              </br>";
+nyanArray[9] = "+      o         o   +       o    </br>";
+nyanArray[10] = "    +         +                   </br>";
+nyanArray[11] = "    o           +                 </br>";
+nyanArray[12] = "+      +     o        o      +    </br>";
+
+var secretArray = new Array();
+secretArray[0] = "There is a \"nyan\" command. I wonder what it will do...";
---
+++ term.js
@@ -0,0 +1,164 @@
+var output = document.getElementById('terminal-output');
+var input = document.getElementById('input');
+
+function addLine(line){
+	output.innerHTML+=line;
+}
+
+function newLine(){
+	addLine("</br>");
+}
+
+function removeLastChar(){
+	output.innerHTML = output.innerHTML.substring(0, output.innerHTML.length - 1);
+}
+
+function clear(){
+	output.innerHTML = "";
+}
+
+
+
+initTerminal();
+
+function getFocus(){
+	input.focus();
+}
+
+function newCommand(event){
+	if (event.keyCode == 13){
+		var command = input.value;
+		input.value = "";
+		addLine("user@malt3.de > "+command+"</br>");
+
+		switch(command)
+		{
+			case "help":
+				addLine("available commands are: \"blog\", \"twitter\", \"github\", \"whoami\", \"clear\", \"secret\" and \"help\" </br>");
+				break;
+			case "blog":
+				var url = "http://blog.malt3.de";
+				var win=window.open(url, '_blank');
+  				win.focus();
+				break;
+			case "twitter":
+				var url = "http://twitter.com/malt3";
+				var win=window.open(url, '_blank');
+  				win.focus();
+				break;
+			case "github":
+				var url = "http://github.com/malt3";
+				var win=window.open(url, '_blank');
+  				win.focus();
+				break;
+			case "clear":
+				clear();
+				break;
+			case "whoami":
+				addLine("Hey visitor, my name is Malte and i am a tech enthusiast from Germany. You can visit my <a href=\"http://blog.malt3.de\">blog</a> to find out more about me.</br>");
+				break;
+			case "secret":
+				random = Math.floor(Math.random() * secretArray.length);
+				console.log(random);
+				addLine(secretArray[random]);
+				break;
+			case "nyan":
+				for (var i = 0; i < nyanArray.length; i++) {
+					addLine(nyanArray[i]);
+				};
+				break;
+			default:
+				addLine("unknown command. Use \"help\" to see available commands</br>");
+				break;
+		}
+	}
+}
+
+function initSystem(){
+	for (var i = 0; i < sysInits.length; i++) {
+		addLine("loading "+sysInits[i]+" .... [OK]</br>");
+	};
+}
+
+function initTerminal(){
+clear();
+for (var i = 0; i <= 15; i++) { //Only show the logo
+	addLine(stringArray[i]);
+};
+
+function flicker(times, callback){
+	if (times > 0){
+		switch(times%7)
+		{
+			case 0:
+				addLine("-");
+    			break;
+    		case 1:
+				addLine("\\");
+    			break;
+    		case 2:
+				addLine("|");
+    			break;
+    		case 3:
+				addLine("/");
+    			break;
+			case 4:
+				addLine("-");
+    			break;
+			case 5:
+				addLine("\\");
+    			break;
+			case 6:
+				addLine("|");
+    			break;
+			case 7:
+				addLine("/");
+    			break;
+    	}
+    	setTimeout(function(){
+			removeLastChar();
+			flicker(times-1, callback);
+    	},100);
+	}else{
+		callback();
+	}
+}
+
+function loading(callback){
+	function addChar(times, callback){
+		if (times > 0) {
+			addLine("=");
+			setTimeout(function(){
+				addChar(times-1, callback);
+    		},90);
+		}else{
+			callback();
+		}
+	}
+	addLine("[");
+	var closeBracket = function(){
+		addLine("]");
+		callback();
+	};
+	addChar(15, closeBracket);
+}
+
+var delay=700;// 0.7 seconds
+setTimeout(function(){
+    addLine(stringArray[16]);
+    loading(function(){
+    	addLine(stringArray[17]);
+    	var cb = function() {
+    	newLine();
+    	initSystem();
+    	addLine(stringArray[18]);
+    	addLine(stringArray[19]);
+    	addLine(stringArray[20]);
+    	document.getElementById('prompt').style.visibility='visible';
+    	getFocus();
+    	};
+    	flicker(25, cb);
+   });
+    
+},delay);
+}
